cac20bcc9bf64d82cd30dc6187ba6150527fc9de
Susheel Kumar <susheelkumar7783@gmail.com>
2022-11-07 23:15:33 +0530

    node js project

---
+++ public/js/main.js
@@ -0,0 +1,98 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
+
+const getInfo = async(event) => {
+    event.preventDefault();
+
+    let cityVal = cityName.value;
+
+    if(cityVal === ""){
+        city_name.innerText = `Plz write the name before search`;
+        datahide.classList.add("data_hide");
+    }else{
+
+        try{
+           
+
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`
+            const response = await fetch(url);
+
+            const data = await response.json();
+            const arrData = [data];
+
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            const tempMood = arrData[0].weather[0].main;
+            console.log(tempMood);
+
+            //condition to check sunny or cloudy
+            if (tempMood == "Clear") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+            } else if (tempMood == "Clouds") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+            } else if (tempMood == "Rain") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+            } else {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
+
+            }
+            datahide.classList.remove('data_hide');
+            cityVal = "";
+           
+       
+        }catch{
+            cityVal = " ";
+            datahide.classList.add("data_hide");
+            city_name.innerText =  `please enter the proper city name`;
+            console.log('please add the proper city name');
+        }
+        
+    }
+}
+
+submitBtn.addEventListener('click', getInfo);
+
+
+// read this
+// for running this program 
+// Windows PowerShell
+// // Copyright (C) Microsoft Corporation. All rights reserved.
+
+// Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+
+//     Directory: C:\Users\SUSHEEL\Desktop\webproject-master
+
+
+// Mode                 LastWriteTime         Length Name                                                                                                                            
+// ----                 -------------         ------ ----                                                                                                                            
+// d-----         11/7/2022   7:40 PM                webproject-master
+
+
+// PS C:\Users\SUSHEEL\Desktop\webproject-master> cd webproject-master
+// PS C:\Users\SUSHEEL\Desktop\webproject-master\webproject-master> nodemon src/app.js
+// [nodemon] 2.0.20
+// [nodemon] to restart at any time, enter `rs`
+// [nodemon] watching path(s): *.*
+// [nodemon] watching extensions: js,mjs,json
+// [nodemon] starting `node src/app.js`
+// listening to the port no at 8000
+
+
+
+
+
+
+
+
+
+
